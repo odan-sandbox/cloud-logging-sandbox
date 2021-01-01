e987508923b2faccd5c7a6c44adc8b40e2e197ce
odan <odan3240@gmail.com>
2021-01-01 21:30:54 +0900

    add google-cloud/logging

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,12 @@
+import { Logging } from "@google-cloud/logging";
+
 async function main(): Promise<void> {
-  console.log("poyo");
+  const projectId = "cloud-logging-sandbox";
+  const logging = new Logging({ projectId });
+
+  const log = logging.log("test");
+
+  log.info(log.entry({ text: "poyo" }));
 }
 
 main();
